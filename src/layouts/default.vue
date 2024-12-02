9e33785a150ca8c6e0d648a4955805059b0f3b72
liang <2775759471@qq.com>
2024-12-02 14:49:08 +0800

    build: 基础框架

---
+++ src/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <view>
+    <slot />
+  </view>
+</template>
+
+<script lang="ts" setup></script>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,51 @@
+<!-- 使用 type="home" 属性设置首页，其他页面不需要设置，默认为page；推荐使用json5，更强大，且允许注释 -->
+<route lang="json5" type="home">
+{
+  style: {
+    navigationStyle: 'custom',
+    navigationBarTitleText: '首页',
+  },
+}
+</route>
+<template>
+  <view class="overflow-hidden" :style="{ marginTop: safeAreaInsets?.top + 'px' }">
+    <navbar :placeholder="true" title="购物车">
+      <template v-slot:right>
+        <div class="light-color edit">完成</div>
+      </template>
+    </navbar>
+    <view class="pt-2 px-4">
+      <view class="text-justify max-w-100 m-auto text-4 indent mb-2">{{ description }}</view>
+      <view class="text-center mt-8">
+        当前平台是：
+        <text class="text-green-500">{{ PLATFORM.platform }}</text>
+      </view>
+      <up-button type="primary" text="确定"></up-button>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import PLATFORM from '@/utils/platform'
+
+defineOptions({
+  name: 'Home',
+})
+
+// 获取屏幕边界到安全区域距离
+const { safeAreaInsets } = uni.getSystemInfoSync()
+const author = ref('菲鸽')
+const description = ref(
+  'unibest 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite4 + UnoCss + UniUI + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
+)
+// 测试 uni API 自动引入
+onLoad(() => {
+  console.log(author)
+})
+</script>
+
+<style>
+.main-title-color {
+  color: #d14328;
+}
+</style>
