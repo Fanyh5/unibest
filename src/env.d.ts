cf4bf663e437a840618e1580535252add5b35df0
liang <2775759471@qq.com>
2024-12-03 15:54:23 +0800

    build: 配置环境变量

--- src/env.d.ts
+++ src/env.d.ts
@@ -23,7 +23,44 @@ interface ImportMetaEnv {
   readonly VITE_UPLOAD_BASEURL: string
   /** 是否清除console */
   readonly VITE_DELETE_CONSOLE: string
-  // 更多环境变量...
+  /** 唤起app地址 */
+  readonly VITE_APP_SCHEME_LINK: string
+  /** 下载地址，下载app的地址 */
+  readonly VITE_APP_DOWNLOAD_LINK: string
+  /** 分享地址，也就是在h5中默认的复制地址 */
+  readonly VITE_APP_SHARE_LINK: string
+  /** 小程序唯一凭证，即 AppID */
+  readonly VITE_APP_ID: string
+  /** 在高德中申请Web服务key */
+  readonly VITE_A_MAP_KEY: string
+  /** 扫码认证跳转域名配置 */
+  readonly VITE_SCAN_AUTH_NAVIGATION: string[]
+  /** AppStore的应用地址id */
+  readonly VITE_IOS_APP_ID: string
+  /** logo地址 */
+  readonly VITE_LOGO: string
+  /** 客服电话 */
+  readonly VITE_CUSTOMER_SERVICE_MOBILE: string
+  /** 客服邮箱 */
+  readonly VITE_CUSTOMER_SERVICE_EMAIL: string
+  /** IM地址 */
+  readonly VITE_IM_WEB_SRC: string
+  /** IM WS 地址 */
+  readonly VITE_BASE_WS_URL: string
+  /** 是否启用粘贴板获取链接 */
+  readonly VITE_ENABLE_GET_CLIPBOARD: boolean
+  /** 是否在h5中右侧浮空按钮点击启动app */
+  readonly VITE_ENABLE_MINI_BAR_START_UP_APP: boolean
+  /** 主题色 */
+  readonly VITE_MAIN_COLOR: string
+  /** 高亮主题色 */
+  readonly VITE_LIGHT_COLOR: string
+  /** 辅助高亮颜色 */
+  readonly VITE_AIDER_LIGHT_COLOR: string
+  /** 默认用户头像 */
+  readonly VITE_DEFAULT_USER_PHOTO: string
+  /** 是否启用获取手机号登录 */
+  readonly VITE_ENABLE_FETCH_MOBILE_LOGIN: boolean
 }
 
 interface ImportMeta {
--- src/typings.ts
+++ src/typings.ts
@@ -23,7 +23,25 @@ type IUserInfo = {
   token?: string
 }
 
-enum TestEnum {
-  A = 'a',
-  B = 'b',
+type IConfig = {
+  name: string
+  schemeLink: string // 唤起app地址
+  downloadLink: string // 下载地址，下载app的地址
+  shareLink: string // 分享地址，也就是在h5中默认的复制地址
+  appid: string // 小程序唯一凭证，即 AppID
+  aMapKey: string // 在高德中申请Web服务key
+  scanAuthNavigation: string[] // 扫码认证跳转域名配置
+  iosAppId: string // AppStore的应用地址id
+  logo: string // logo地址
+  customerServiceMobile: string // 客服电话
+  customerServiceEmail: string // 客服邮箱
+  imWebSrc: string // IM地址
+  baseWsUrl: string // IM WS 地址
+  enableGetClipboard: boolean // 是否启用粘贴板获取链接
+  enableMiniBarStartUpApp: boolean // 是否在h5中右侧浮空按钮点击启动app
+  mainColor: string // 主题色
+  lightColor: string // 高亮主题色
+  aiderLightColor: string // 辅助高亮颜色
+  defaultUserPhoto: string // 默认用户头像
+  enableFetchMobileLogin: boolean // 是否启用获取手机号登录
 }
